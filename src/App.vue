92c5928396dfa5310195febe0527e10791001071
Leonardo Lucas de Oliveira <leonardolucasdeoliveira4@gmail.com>
2026-05-18 21:24:30 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Cabecalho from "./components/Cabecalho.vue";
+import TabelaLivros from "./components/TabelaLivros.vue";
+
+const sidebarAberta = ref(false);
+
+const toggleSidebar = () => {
+  sidebarAberta.value = !sidebarAberta.value;
+};
+
+const fecharSidebar = () => {
+  sidebarAberta.value = false;
+};
+</script>
+
+<template>
+  <Cabecalho />
+  <BarraLateral
+    :isOpen="sidebarAberta"
+    @toggle="toggleSidebar"
+    @close="fecharSidebar"
+  />
+  <TabelaLivros/>
+</template>
